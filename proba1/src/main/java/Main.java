4d0b86166a1040226dc42c8b231825177aa980b3
Kokin Andrey <akokin-cbgr@users.noreply.github.com>
2019-02-15 17:38:03 +0300

    Доработка функций

--- proba1/src/main/java/Main.java
+++ proba1/src/main/java/Main.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
-    Point p1 = new Point(1,1);
-    System.out.println("Создана точка с координатами");
-    Point p2 = new Point(2,2);
-    System.out.print("Расстояние между точками p1 и p2 = ");
-    System.out.println(distance(p1,p2));
+    System.out.println("Расстояние между точками p1 и p2 на плоскости рассчитывается согласно формуле "+ "\n" + "AB = √((xb - xa)*2 + (yb - ya)*2)" + "\n");
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Введите координаты X точки р1" + "\n" + "x = ");
+    int x = sc.nextInt();
+    System.out.print("Введите координаты Y точки р1"+ "\n" + "y = ");
+    int y = sc.nextInt();
+    Point p1 = new Point(x,y);
+    System.out.println("Создана точка p1 с координатами x = " + p1.x + " y = " + p1.y + "\n" + "\n");
+
+
+
+    System.out.print("Введите координаты X точки р2" + "\n" + "x = ");
+    int a = sc.nextInt();
+    System.out.print("Введите координаты Y точки р2"+ "\n" + "y = ");
+    int b = sc.nextInt();
+    Point p2 = new Point(a,b);
+    System.out.println("Создана точка p2 с координатами x = " + p2.x + " y = " + p2.y + "\n" + "\n");
+
+    System.out.println("Вычисление расстояния между точками p1 и p2" + "\n" + "AB = " + distance(p1,p2));
   }
   public static double distance(Point p1, Point p2){
     return Math.sqrt(Math.pow((p2.x - p1.x),2) + Math.pow((p2.y - p1.y),2));
