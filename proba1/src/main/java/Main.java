ef0181d769094f339525721afed1ef4d97c87cce
Kokin Andrey <akokin-cbgr@users.noreply.github.com>
2019-02-15 17:12:17 +0300

    Реализация методов

--- proba1/src/main/java/Main.java
+++ proba1/src/main/java/Main.java
@@ -1,5 +1,14 @@
+
 public class Main {
+
   public static void main(String[] args) {
-    System.out.println("111");
+    Point p1 = new Point(1,1);
+    System.out.println("Создана точка с координатами");
+    Point p2 = new Point(2,2);
+    System.out.print("Расстояние между точками p1 и p2 = ");
+    System.out.println(distance(p1,p2));
+  }
+  public static double distance(Point p1, Point p2){
+    return Math.sqrt(Math.pow((p2.x - p1.x),2) + Math.pow((p2.y - p1.y),2));
   }
 }
