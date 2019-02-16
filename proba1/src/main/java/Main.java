016e005f4a42b6f4b13e66e85f054b6b8e01490c
Kokin Andrey <akokin-cbgr@users.noreply.github.com>
2019-02-16 15:24:39 +0300

    добавлена инкапсуляция в класс Point

--- proba1/src/main/java/Main.java
+++ proba1/src/main/java/Main.java
@@ -3,35 +3,38 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Расстояние между точками p1 и p2 на плоскости рассчитывается согласно формуле "+ "\n" + "AB = √((xb - xa)*2 + (yb - ya)*2)" + "\n");
+    System.out.println("\n" + "\n" + "Расстояние между точками p1 и p2 на плоскости рассчитывается согласно формуле "+ "\n" + "AB = √((xb - xa)*2 + (yb - ya)*2)" + "\n");
 
     Scanner sc = new Scanner(System.in);
 
     System.out.print("Введите координаты X точки р1" + "\n" + "x = ");
     while (!sc.hasNextInt()){
-      System.out.println("Введено не число! ");
+      System.out.println("Ошибка ввода. Введено не число! ");
       sc.next();
+      System.out.print("\n" + "Введите координаты X точки р1" + "\n" + "x = ");
     }
     int x = sc.nextInt();
 
 
     System.out.print("Введите координаты Y точки р1"+ "\n" + "y = ");
     while (!sc.hasNextInt()){
-      System.out.println("Введено не число! ");
+      System.out.println("Ошибка ввода. Введено не число! ");
       sc.next();
+      System.out.print("\n" + "Введите координаты Y точки р1" + "\n" + "y = ");
     }
     int y = sc.nextInt();
 
 
     Point p1 = new Point(x,y);
-    System.out.println("Создана точка p1 с координатами x = " + p1.x + " y = " + p1.y + "\n" + "\n");
+    System.out.println("Создана точка p1 с координатами x = " + p1.getX() + ", y = " + p1.getY() + "\n" + "\n");
 
 
 
     System.out.print("Введите координаты X точки р2" + "\n" + "x = ");
     while (!sc.hasNextInt()){
-      System.out.println("Введено не число! ");
+      System.out.println("Ошибка ввода. Введено не число! ");
       sc.next();
+      System.out.print("\n" + "Введите координаты X точки р2" + "\n" + "x = ");
     }
     int a = sc.nextInt();
 
@@ -39,15 +42,16 @@ public class Main {
 
     System.out.print("Введите координаты Y точки р2" + "\n" + "y = ");
     while (!sc.hasNextInt()){
-        System.out.println("Введено не число! ");
+        System.out.println("Ошибка ввода. Введено не число! ");
         sc.next();
+        System.out.print("\n" + "Введите координаты Y точки р2" + "\n" + "y = ");
     }
     int b = sc.nextInt();
 
 
     Point p2 = new Point(a,b);
-    System.out.println("Создана точка p2 с координатами x = " + p2.x + " y = " + p2.y + "\n" + "\n");
+    System.out.println("Создана точка p2 с координатами x = " + p2.getX() + ", y = " + p2.getY() + "\n" + "\n");
 
-    System.out.println("Вычисление расстояния между точками p1 и p2" + "\n" + "AB = " + p1.distance(p1,p2));
+    System.out.println("Вычисление расстояния между точками p1(x:" + p1.getX() + ", y:" + p1.getY() + ") и p2(x:" + p2.getX() + ", y:" + p2.getY() + ")\n" + "AB = " + p1.distance(p1,p2));
   }
 }
--- proba1/src/main/java/Point.java
+++ proba1/src/main/java/Point.java
@@ -1,6 +1,14 @@
 public class Point {
-  int x;
-  int y;
+  private int x;
+  private int y;
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
 
   public Point(int x, int y){
     this.x = x;
