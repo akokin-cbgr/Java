4d1c3ab6fc67473b46f2957678b3f3fa42cc108a
Kokin Andrey <akokin-cbgr@users.noreply.github.com>
2019-03-04 09:35:43 +0300

    Отладка выполнения ДЗ Лекции 4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -11,7 +11,7 @@ public class TestBase {
   public TestBase() {
   }
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
