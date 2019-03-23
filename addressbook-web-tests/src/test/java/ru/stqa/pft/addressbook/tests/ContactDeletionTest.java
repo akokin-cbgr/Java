1925bbb4487404c1bb5264300af1c19afe248e21
Kokin Andrey <akokin-cbgr@users.noreply.github.com>
2019-03-23 20:41:34 +0300

    Модификация тестов удаления контактов и групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
@@ -4,8 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class ContactDeletionTest extends TestBase {
@@ -21,14 +23,12 @@ public class ContactDeletionTest extends TestBase {
 
   @Test
   public void testContactDeletionTests() throws Exception {
-    Set<ContactData> before = app.сontact().all();
+    Contacts before = app.сontact().all();
     ContactData deletedContact = before.iterator().next();
     app.сontact().deletionContact(deletedContact);
-    Set<ContactData> after = app.сontact().all();
-    Assert.assertEquals(after.size(), before.size() - 1);
-
-    before.remove(deletedContact);
-    Assert.assertEquals(before, after);
+    Contacts after = app.сontact().all();
+    assertThat(after.size(), equalTo( before.size() - 1));
+    assertThat(after, equalTo(before.without(deletedContact)));
 
 
 
