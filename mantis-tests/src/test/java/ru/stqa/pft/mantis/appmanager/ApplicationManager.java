376a025be4ce0cfc58f3ce4ac1f5760f296c4351
Kokin Andrey <akokin-cbgr@users.noreply.github.com>
2019-04-14 16:46:28 +0300

    Лекция 8 Урок 4

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -13,17 +12,13 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import static org.testng.Assert.fail;
-
 public class ApplicationManager {
+
   private final Properties properties;
   private WebDriver driver;
 
-
-
-  private boolean acceptNextAlert;
-  private StringBuffer verificationErrors = new StringBuffer();
   private String browser;
+  private RegistrationHelper registrationHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -33,53 +28,42 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/properties/%s.properties", target))));
-    if (browser.equals(BrowserType.FIREFOX)) {
-      driver = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      driver = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      driver = new InternetExplorerDriver();
-    }
-
-
-    driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    driver.get(properties.getProperty("web.baseUrl"));
   }
 
   public void stop() {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
+    if (driver != null) {
+      driver.quit();
     }
   }
 
-
-  public String closeAlertAndGetItsText() throws InterruptedException {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
-
-  public void setAcceptNextAlert(boolean acceptNextAlert) {
-    this.acceptNextAlert = acceptNextAlert;
-  }
-
-  public HttpSession newSession(){
+  public HttpSession newSession() {
     return new HttpSession(this);
   }
 
 
   public String getProperty(String key) {
     return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() {
+    if (driver == null) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        driver = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        driver = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        driver = new InternetExplorerDriver();
+      }
+      driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+      driver.get(properties.getProperty("web.baseUrl"));
+    }
+    return driver;
   }
 }
