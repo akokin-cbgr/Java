b00f5ab82c7c56fd301b188dde0302e8255c155b
Kokin Andrey <akokin-cbgr@users.noreply.github.com>
2019-04-14 16:15:48 +0300

    Реализован тест для входа в Mantis без браузера, по протоколу HTTP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -74,5 +74,12 @@ public class ApplicationManager {
     this.acceptNextAlert = acceptNextAlert;
   }
 
+  public HttpSession newSession(){
+    return new HttpSession(this);
+  }
 
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
 }
